d999cb01b09480bca4db0fdc739194542c304a76
Jonathan Porter <mail@j.porter.name>
2026-04-15 12:43:50 -0700

    Refactor BaseEntity and WaterHeater classes for improved configuration handling and MQTT publishing

--- energysmartbridge/src/number.js
+++ energysmartbridge/src/number.js
@@ -1,17 +1,15 @@
-import { BaseSensor } from './baseSensor.js';
-import { LOGGER } from './logger.js';
-import { DEVICE_CLASS_MAPPING, READABLE_MAPPING } from './mappings.js';
+import { BaseEntity } from './baseEntity.js';
 
-export class NumberSensor extends BaseSensor {
+export class Number extends BaseEntity {
     sensorType = "number";
     min;
     max;
 
-    constructor (name, waterHeater, value, mqtt, options = {}) {
-        super(name, waterHeater, value, mqtt, options);
+    constructor (name, waterHeater, value, mqtt, config = {}, min, max) {
+        super(name, waterHeater, value, mqtt, config);
 
-        this.max = options.max || 100;
-        this.min = options.min || 1;
+        this.max = max;
+        this.min = min;
     }
 
     commandTopic () {
@@ -26,30 +24,11 @@ export class NumberSensor extends BaseSensor {
         await this.publishState();
     }
 
-    async publishConfig () {
-        const payload = {
-            state_topic: this.createStateTopic(),
-            unique_id: `${this.waterHeater.deviceId}-${this.name}`,
-            name: READABLE_MAPPING[this.name],
-            object_id: `${this.waterHeater.deviceId}_${READABLE_MAPPING[this.name].replaceAll(" ", "_")}`,
+    composeConfig () {
+        return super.composeConfig({
             command_topic: this.commandTopic(),
             min: this.min,
             max: this.max,
-            ...this.waterHeater.generateDeviceConfig(),
-        };
-
-        if (this.diagnostic) {
-            payload.entity_category = 'diagnostic'
-        }
-
-        if (this.name in DEVICE_CLASS_MAPPING) {
-            payload.device_class = DEVICE_CLASS_MAPPING[this.name];
-        }
-
-        const topic = this.createConfigTopic();
-        
-        LOGGER.trace({message: "Publishing config", topic, name: this.name});
-
-        await this.mqtt.publish(topic, JSON.stringify(payload));
+        });
     }
 }
